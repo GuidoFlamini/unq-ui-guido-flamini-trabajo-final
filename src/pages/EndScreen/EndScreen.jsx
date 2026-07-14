7a871e9955ab9273f2132aff893210103e9fa399
Guido N Flamini <gflamini2000@gmail.com>
2026-07-14 19:21:55 -0300

    se crea btn-leaderboard

--- src/pages/EndScreen/EndScreen.jsx
+++ src/pages/EndScreen/EndScreen.jsx
@@ -26,7 +26,7 @@ function EndScreen({ chain, score, onPlayAgain }) {
         <button className="btn-primary" onClick={onPlayAgain}>
           Ir al inicio
         </button>
-        <Link to="/leaderboard" className="btn-secondary">
+        <Link to="/leaderboard" className="btn-leaderboard">
           Ver leaderboard
         </Link>
       </div>
