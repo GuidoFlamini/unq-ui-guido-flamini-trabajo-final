9cb8949142096ddcb6b0e12a739fafcba65ac17e
Guido N Flamini <gflamini2000@gmail.com>
2026-07-14 19:39:00 -0300

    cambio color botones

--- src/pages/EndScreen/EndScreen.jsx
+++ src/pages/EndScreen/EndScreen.jsx
@@ -26,7 +26,7 @@ function EndScreen({ chain, score, onPlayAgain }) {
         <button className="btn-primary" onClick={onPlayAgain}>
           Ir al inicio
         </button>
-        <Link to="/leaderboard" className="btn-leaderboard">
+        <Link to="/leaderboard" className="btn-secondary btn-leaderboard">
           Ver leaderboard
         </Link>
       </div>
